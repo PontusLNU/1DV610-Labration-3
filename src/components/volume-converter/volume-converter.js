d08dff6a6ce3adde948837a06bf3c0b5b435a705
Pontus Serrander <ps223qb@student.lnu.se>
2024-10-25 01:57:27 +0200

    Add HTML to volume-converter.

--- src/components/volume-converter/volume-converter.js
+++ src/components/volume-converter/volume-converter.js
@@ -1,6 +1,27 @@
+import { VolumeConverter } from "unit-converter-package"
+
 const template = document.createElement('template')
 template.innerHTML = `
 <style></style>
+<div class="volume-converter">
+  <h1>Volume Converter</h1>
+  <label for="inputValue">Add a value to convert:</label>
+  <input type="number" id="inputValue" placeholder="Enter value">
+
+  <label for="fromUnit">From:</label>
+  <select id="fromUnit">
+    <option value="gallon" selected>Gallon</option>
+    <option value="litre">Litre</option>
+  </select>
+
+  <label for="toUnit">To:</label>
+  <select id="toUnit">
+    <option value="gallon">Gallon</option>
+    <option value="litre" selected>Litre</option>
+  </select>
+
+  <button id="convert">Convert</button>
+</div>
 `
 customElements.define('volume-converter',
   class extends HTMLElement {
