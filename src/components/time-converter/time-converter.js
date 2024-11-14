ba75c213e7bdcdb0db420af57d70bea6aa6bc0c4
Pontus Serrander <ps223qb@student.lnu.se>
2024-11-15 00:47:23 +0100

    Create the logic for the time-converter.

--- src/components/time-converter/time-converter.js
+++ src/components/time-converter/time-converter.js
@@ -1,10 +1,12 @@
+import { TimeConverter } from "unit-converter-package"
+
 const template = document.createElement('template')
 template.innerHTML = `
 <style></style>
 <div class="time-converter">
   <h1>Time Converter</h1>
-  <label for="inputValue">Enter the time to convert:</label>
-  <input id="inputValue" placeholder="Enter value">
+  <label for="input">Enter the time to convert:</label>
+  <input id="input" placeholder="Enter value">
 
   <label for="fromUnit">From:</label>
   <select id="fromUnit">
@@ -19,6 +21,8 @@ template.innerHTML = `
   </select>
 
   <button id="convert">Convert</button>
+
+  <p id="output"></p>
 </div>
 `
 customElements.define('time-converter',
@@ -26,13 +30,72 @@ customElements.define('time-converter',
 
     #timeConverter
 
+    #convertButton
+
+    #input
+
+    #output
+
+    #fromUnit
+
+    #toUnit
+
     constructor() {
       super()
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-      this.#timeConverter = this.shadowRoot.querySelector('.time-converter')
+      this.#timeConverter = new TimeConverter()
+      this.#convertButton = this.shadowRoot.querySelector('#convert')
+      this.#input = this.shadowRoot.querySelector('#input')
+      this.#output = this.shadowRoot.querySelector('#output')
+      this.#fromUnit = this.shadowRoot.querySelector('#fromUnit')
+      this.#toUnit = this.shadowRoot.querySelector('#toUnit')
+    }
+
+    connectedCallback() {
+      this.#convertButton.addEventListener('click', this.#handleConvert.bind(this))
+    }
+
+    #handleConvert() {
+      try {
+      if (this.#input.value === '') {
+        this.#handleEmptyInput()
+      } else if (this.#fromUnit.value === this.#toUnit.value) {
+        this.#handleSameUnitConversion()
+      } else if (this.#fromUnit.value === '12h' && this.#toUnit.value === '24h') {
+        this.#handle12hTo24hConversion()
+      } else if (this.#fromUnit.value === '24h' && this.#toUnit.value === '12h') {
+        this.#handle24hTo12hConversion()
+      }
+    } catch (error) {
+      this.#handleError()
+    }
+  }
+
+    #handleEmptyInput() {
+      this.#output.textContent = 'Please enter a time in valid format.'
+    }
+
+    #handleSameUnitConversion( ) {
+      this.#output.textContent = `Please select different time formats to convert.`
+    }
+
+    #handle12hTo24hConversion() {
+      this.#output.textContent = `${this.#input.value} = ${this.#timeConverter.convert12HourTo24Hour(this.#input.value).toString()}`
+    }
+
+    #handle24hTo12hConversion() {
+      this.#output.textContent = `${this.#input.value} = ${this.#timeConverter.convert24HourTo12Hour(this.#input.value).toString()}`
+    }
+
+    #handleError() {
+      this.#output.textContent = 'Please enter the time in a valid format. Do not forget PM/AM for 12 hours conversion.'
+    }
+
+    clearOutput() {
+      this.#output.textContent = ''
     }
   }
 )
