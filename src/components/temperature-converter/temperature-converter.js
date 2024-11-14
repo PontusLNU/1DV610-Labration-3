5adab8e753ec8f5a961354a01a6ea3663036d3f9
Pontus Serrander <ps223qb@student.lnu.se>
2024-11-15 00:46:13 +0100

    Add the logic for the temperature converter.

--- src/components/temperature-converter/temperature-converter.js
+++ src/components/temperature-converter/temperature-converter.js
@@ -1,10 +1,12 @@
+import { TemperatureConverter } from "unit-converter-package"
+
 const template = document.createElement('template')
 template.innerHTML = `
 <style></style>
 <div class="temperature-converter">
   <h1>Temperature Converter</h1>
-  <label for="inputValue">Add a value to convert:</label>
-  <input type="number" id="inputValue" placeholder="Enter value">
+  <label for="input">Add a value to convert:</label>
+  <input type="number" id="input" placeholder="Enter value">
 
   <label for="fromUnit">From:</label>
   <select id="fromUnit">
@@ -19,6 +21,8 @@ template.innerHTML = `
   </select>
 
   <button id="convert">Convert</button>
+
+  <p id="output"></p>
 </div>
 `
 customElements.define('temperature-converter',
@@ -26,13 +30,64 @@ customElements.define('temperature-converter',
 
     #temperatureConverter
 
+    #convertButton
+
+    #input
+
+    #output
+
+    #fromUnit
+
+    #toUnit
+
     constructor() {
       super()
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-      this.#temperatureConverter = this.shadowRoot.querySelector('.temperature-converter')
+      this.#temperatureConverter =  new TemperatureConverter()
+      this.#convertButton = this.shadowRoot.querySelector('#convert')
+      this.#input = this.shadowRoot.querySelector('#input')
+      this.#output = this.shadowRoot.querySelector('#output')
+      this.#fromUnit = this.shadowRoot.querySelector('#fromUnit')
+      this.#toUnit = this.shadowRoot.querySelector('#toUnit')
+    }
+
+    connectedCallback() {
+      this.#convertButton.addEventListener('click', this.#handleConvert.bind(this))
+    }
+
+    #handleConvert() {
+      if (this.#input.value === '') {
+        this.#handleEmptyInput()
+      } else if (this.#fromUnit.value === this.#toUnit.value) {
+        this.#handleSameUnitConversion()
+      } else if (this.#fromUnit.value === 'celsius' && this.#toUnit.value === 'fahrenheit') {
+        this.#handleCelsiusToFahrenheitConversion()
+      } else if (this.#fromUnit.value === 'fahrenheit' && this.#toUnit.value === 'celsius') {
+        this.#handleFahrenheitToCelsiusConversion()
+      }
+    }
+
+    #handleSameUnitConversion( ) {
+      this.#output.textContent = `${this.#input.value} degrees ${this.#fromUnit.value} is still ${this.#input.value} degrees ${this.#fromUnit.value} Please select different units to convert.`
+    }
+
+    #handleCelsiusToFahrenheitConversion() {
+      this.#output.textContent = `${this.#input.value} degrees ${this.#fromUnit.value}  = ${this.#temperatureConverter.convertCelsiusToFahrenheit(parseFloat(this.#input.value))} degrees ${this.#toUnit.value}`
+    }
+
+    #handleFahrenheitToCelsiusConversion() {
+      this.#output.textContent = `${this.#input.value} degrees ${this.#fromUnit.value}  = ${this.#temperatureConverter.convertFahrenheitToCelsius(parseFloat(this.#input.value))} degrees ${this.#toUnit.value}`
+    }
+
+    #handleEmptyInput() {
+      this.#output.textContent = 'Please enter a value to convert.'
+    }
+
+    clearOutput() {
+      this.#output.textContent = ''
     }
   }
 )
